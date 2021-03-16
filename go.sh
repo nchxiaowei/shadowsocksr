4ec4542c306afbb43790c5d043a22f1a72696a4b
nchxiaowei <nchxiaoweI@sina.com>
2021-03-16 19:40:20 +0800

    Add files via upload

---
+++ go.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+
+sudo su
+git clone https://github.com/nchxiaowei/shadowsocksr.git
+cd shadowsocksr/
+chmod 777 bbr
+sh bbr
+
+cp rc.local /etc/
+chmod +x /etc/rc.local
+cp rc-local.service /etc/systemd/system/
+systemctl enable rc-local.service
+
+reboot
+echo succ
