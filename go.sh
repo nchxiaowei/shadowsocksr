28490f0ead5715a0d783a7b8a923e7f4af670f8c
nchxiaowei <nchxiaoweI@sina.com>
2021-03-24 16:20:43 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -3,12 +3,9 @@
 #git clone https://github.com/nchxiaowei/shadowsocksr.git
 #cd shadowsocksr/
 dirpath=$(cd `dirname $0`;pwd)
-
 chmod 777 bbr
 sh bbr
 cp -f rc.local /etc/
 chmod +x /etc/rc.local
-cp -f rc-local.service /etc/systemd/system/
-systemctl enable rc-local.service
-reboot
+#reboot
 echo succ
